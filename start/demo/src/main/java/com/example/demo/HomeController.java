ce6c92df2d0dbcfd8f2e70ba6bcbcd1b91efe918
SHLIM1103 <gusl5525@gmail.com>
2020-11-26 14:44:12 +0900

    first

--- start/demo/src/main/java/com/example/demo/HomeController.java
+++ start/demo/src/main/java/com/example/demo/HomeController.java
@@ -1,5 +1,12 @@
 package com.example.demo;
 
-public class HomeController{
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
+@Controller
+public class HomeController {
+    @GetMapping("/")
+    public String index(){
+        return "index";
+    }
 }
